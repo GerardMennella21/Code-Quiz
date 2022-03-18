6deff0d716d9504413eee19f20185a3297d9a2d9
Gerard Mennella <gerardmennella21@gmail.com>
2022-03-17 23:38:36 -0400

    Structuring

---
+++ assets/js/script.js
@@ -0,0 +1,27 @@
+var questions = [
+    {
+        question: "What is a pseudo-class?",
+        options: ["A CSS rule that contains no declarations.","A CSS declaration that hides the element.","An element that has more than one class.","A CSS keyword to target an element's state."],
+        answer: "A CSS keyword to target an element's state."
+    },
+    {
+        question:"Which of the following is an example of a pseudo-element?",
+        options: ["::before","::after","::first-letter","all of the above"],
+        answer: "all of the above"
+    },
+    {
+        question:"What does the * mean in css?",
+        options: ["single selector","multiple selector","inverse selector","universal selector"],
+        answer: "universal selector"
+    },
+    {
+        question:"what does the Z-index property do?",
+        options: ["Removes an element from the DOM","changes the stacking order of elements","Changes the opacity of an element.","Forces an element to be positioned relatively."],
+        answer: "changes the stacking order of elements"
+    },
+    {
+        question:"Using flex if you align-items flex-end where will the items go?",
+        options: ["The left","The top","The bottom","The right"],
+        answer: "The bottom"
+    }
+]
